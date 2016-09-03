bb0154aace003f8c10662f810441b82c9f6e9da4
falgon <falgon53@yahoo.co.jp>
2016-09-04 02:50:13 +0900

    setup

---
+++ srook/iterator/emplace_back_insert_iterator.hpp
@@ -0,0 +1,58 @@
+#ifndef INCLUDED_EMPLACE_BACK_INSERT_ITERATOR
+#define INCLUDED_EMPLACE_BACK_INSERT_ITERATOR
+#ifdef  __GXX_EXPERIMENTAL_CXX0X__
+#include<iterator>
+namespace srook{
+inline namespace v1{
+
+template<class Container>
+class emplace_back_insert_iterator:
+	public std::iterator<std::output_iterator_tag,void,void,void,void>{
+protected:
+	Container* container;
+public:
+	typedef Container container_type;
+
+	explicit emplace_back_insert_iterator(Container& x)
+		:container(&x)
+	{}
+	emplace_back_insert_iterator&
+	operator=(typename Container::value_type& value)
+	{
+		container->emplace_back(value);
+		return *this;
+	}
+	emplace_back_insert_iterator&
+	operator=(typename Container::value_type&& value)
+    {
+		container->emplace_back(std::move(value));
+		return *this;
+	}
+	emplace_back_insert_iterator&
+	operator*()
+	{
+		return *this;
+	}
+	emplace_back_insert_iterator&
+	operator++()
+	{
+		return *this;
+	}
+	emplace_back_insert_iterator&
+	operator++(int)
+	{
+		return *this;
+	}
+};
+
+template<class Container>
+constexpr srook::emplace_back_insert_iterator<Container> 
+emplace_back_inserter(Container& container)
+{
+	return srook::emplace_back_insert_iterator<Container>(container);
+}
+
+}
+}
+#endif
+#endif
---
+++ srook/string/str_replace.hpp
@@ -0,0 +1,24 @@
+#ifndef INCLUDED_STRING_REPLACE
+#define INLCUDED_STRING_REPLACE
+#include<string>
+#include<cstring>
+
+namespace miko{
+namespace v1{
+
+std::string& str_replace(std::string& str,const char* src,const char* rhs)
+{
+	if(str.empty()||!src||!*src||!rhs||!strcmp(src,rhs))
+		return str;
+	const size_t src_len=strlen(src);
+	const size_t rhs_len=strlen(rhs);
+	for(std::string::size_type i=0;
+			(i=str.find(src,i,src_len))!=std::string::npos;
+			i+=rhs_len)
+		str.replace(i,src_len,rhs,rhs_len);
+	return str;
+}
+
+}
+}
+#endif
