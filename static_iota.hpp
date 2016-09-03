0bc975d6e7d6d66eabf81e5bef190c46e6a699e6
Roki <falgon53@yahoo.co.jp>
2016-09-04 02:13:58 +0900

    Delete static_iota.hpp

--- static_iota.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDED_STATIC_IOTA
-#define INCLUDED_STATIC_IOTA
-#include<boost/preprocessor/repetition/repeat_from_to.hpp>
-#define static_iota_t(z,x,text) x,
-#define static_iota(x,y) BOOST_PP_REPEAT_FROM_TO(x,y,static_iota_t,_)
-#define IIOTA static_iota
-#define INIT_IOTA static_iota
-#endif
