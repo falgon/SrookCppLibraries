bfed2d991c5e2d4191cc71c0b438a93c7df48b6c
Roki <falgon53@yahoo.co.jp>
2016-09-08 02:44:43 +0900

    Update str_replace.hpp

--- srook/string/str_replace.hpp
+++ srook/string/str_replace.hpp
@@ -8,10 +8,10 @@ inline namespace v1{
 	
 std::string& str_replace(std::string& str,const char* src,const char* rhs)
 {
-	if(str.empty()||!src||!*src||!rhs||!strcmp(src,rhs))
-		return str;
-	const size_t src_len=strlen(src);
-	const size_t rhs_len=strlen(rhs);
+	if(str.empty()||!src||!*src||!rhs||!std::strcmp(src,rhs))return str;
+	
+	const size_t src_len=std::strlen(src);
+	const size_t rhs_len=std::strlen(rhs);
 	for(std::string::size_type i=0;
 			(i=str.find(src,i,src_len))!=std::string::npos;
 			i+=rhs_len)
