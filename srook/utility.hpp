cd8adaa5e28e42d1869bcf14eb25c8182963a10b
falgon <falgon53@yahoo.co.jp>
2017-01-16 23:34:18 +0900

    add in_place

--- srook/utility.hpp
+++ srook/utility.hpp
@@ -1,4 +1,5 @@
 #ifndef INCLUDED_UTILITY_HPP
 #define INCLUDED_UTILITY_HPP
 #include<srook/utility/initialize_iota.hpp>
+#include<srook/utility/in_place.hpp>
 #endif
---
+++ srook/utility/in_place.hpp
@@ -0,0 +1,25 @@
+#ifndef INCLUDED_SROOK_UTILITY_IN_PLACE_HPP
+#define INCLUDED_SROOK_UTILITY_IN_PLACE_HPP
+#include<cstddef>
+namespace srook{
+inline namespace v1{
+constexpr struct in_place_t{
+	explicit in_place_t()=default;
+}in_place{};
+
+template<class T>
+struct in_place_type_t{
+	explicit in_place_type_t()=default;
+};
+template<class T>
+constexpr in_place_type_t<T> in_place_type{};
+
+template<std::size_t I>
+struct in_place_index_t{
+	explicit in_place_index_t()=default;
+};
+template<std::size_t I>
+constexpr in_place_index_t<I> in_place_index{};
+} // inline namespace v1
+} // srook
+#endif
