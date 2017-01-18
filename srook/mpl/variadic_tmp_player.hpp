c322fbd6ad8f317a22ee63750d7c7322ce6e72ac
falgon <falgon53@yahoo.co.jp>
2017-01-19 01:42:30 +0900

    add swap_specified feauture

--- srook/mpl/variadic_tmp_player.hpp
+++ srook/mpl/variadic_tmp_player.hpp
@@ -288,9 +288,17 @@ struct At<index,Head,Tail...>{
 	using type=typename At<index-1,Tail...>::type;
 };
 template<class Target,class... Tail>
+struct At<0,pack<Target,Tail...>>{
+	using type=Target;
+};
+template<class Target,class... Tail>
 struct At<0,Target,Tail...>{
 	using type=Target;
 };
+/*template<class Target>
+struct At<0,pack<Target>>{
+	using type=Target;
+};*/
 template<std::size_t index,class Head,class... Tail>
 struct At<index,pack<Head,Tail...>>:At<index,Head,Tail...>{};
 template<std::size_t index,class... Pack>
@@ -313,6 +321,102 @@ template<class LPack,class RPack,std::size_t IndexTarget>
 using SwapAt_L=typename SwapAt<LPack,RPack,IndexTarget,pack<>,pack<>>::L_type;
 template<class LPack,class RPack,std::size_t IndexTarget>
 using SwapAt_R=typename SwapAt<LPack,RPack,IndexTarget,pack<>,pack<>>::R_type;
+
+
+// SwapAt_Specified
+template<class,std::size_t,class,std::size_t,class,class,std::size_t,std::size_t>
+struct SwapAt_Specified_L_t;
+
+template<class,std::size_t,class,std::size_t,class,class,std::size_t,std::size_t>
+struct SwapAt_Specified_R_t;
+
+template<
+	class LH,class... LT,std::size_t IL,
+	class... R,std::size_t IR,
+	class LHeads,class RHeads,
+	std::size_t CORE_IL,std::size_t CORE_IR
+>
+struct SwapAt_Specified_L_t<
+		pack<LH,LT...>,IL,
+		pack<R...>,IR,
+		LHeads,RHeads,
+		CORE_IL,CORE_IR
+>{
+	using L_type=
+		typename SwapAt_Specified_L_t<
+			pack<LT...>,IL-1,
+			pack<R...>,IR,
+			Concat_t<LH,LHeads>,pack<R...>,
+			CORE_IL,CORE_IR
+		>::L_type;
+};
+
+template<
+	class... L,std::size_t IL,
+	class RH,class... RT,std::size_t IR,
+	class LHeads,class RHeads,
+	std::size_t CORE_IL,std::size_t CORE_IR
+>
+struct SwapAt_Specified_R_t<
+		pack<L...>,IL,
+		pack<RH,RT...>,IR,
+		LHeads,RHeads,
+		CORE_IL,CORE_IR
+>{
+	using R_type=
+		typename SwapAt_Specified_R_t<
+			pack<L...>,IL,
+			pack<RT...>,IR-1,
+			pack<L...>,Concat_t<RH,RHeads>,
+			CORE_IL,CORE_IR
+		>::R_type;
+};
+
+template<
+	class LH,class... LT,
+	class... R,std::size_t IR,
+	class LHeads,class RHeads,
+	std::size_t CORE_IL,std::size_t CORE_IR
+>
+struct SwapAt_Specified_L_t<
+		pack<LH,LT...>,0,
+		pack<R...>,IR,
+		LHeads,RHeads,
+		CORE_IL,CORE_IR
+>{
+	using L_type=
+		Concat_t<
+			Concat_t<
+				LHeads,
+				At_t<CORE_IR,pack<R...>>
+			>,
+		LT...>;
+};
+template<
+	class... L,std::size_t IL,
+	class RH,class... RT,
+	class LHeads,class RHeads,
+	std::size_t CORE_IL,std::size_t CORE_IR
+>
+struct SwapAt_Specified_R_t<
+		pack<L...>,IL,
+		pack<RH,RT...>,0,
+		LHeads,RHeads,
+		CORE_IL,CORE_IR
+>{
+		using R_type=
+			Concat_t<
+				Concat_t<
+					RHeads,
+					At_t<CORE_IL,pack<L...>>
+				>,
+			RT...>;
+};
+
+template<class LPack,std::size_t IL,class RPack,std::size_t IR>
+using SwapAt_Specified_L=typename SwapAt_Specified_L_t<LPack,IL,RPack,IR,pack<>,pack<>,IL,IR>::L_type;
+template<class LPack,std::size_t IL,class RPack,std::size_t IR>
+using SwapAt_Specified_R=typename SwapAt_Specified_R_t<LPack,IL,RPack,IR,pack<>,pack<>,IL,IR>::R_type;
 
 } // inline namespace v1
 } // namespace mpl
