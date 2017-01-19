7a124d72dffa21c0e13daa38956a78488d38154f
falgon <falgon53@yahoo.co.jp>
2017-01-19 19:59:23 +0900

    add copy_if

---
+++ srook/range/adaptor/copy_if.hpp
@@ -0,0 +1,46 @@
+#ifndef INCLUDED_SROOK_ADAPTOR_COPY_IF
+#define INCLUDED_SROOK_ADAPTOR_COPY_IF
+#include<srook/range/adaptor/adaptor_operator.hpp>
+#include<srook/iterator/range_iterator.hpp>
+#if __has_include(<boost/range/algorithm/copy_if.hpp>)
+#define POSSIBLE_TO_BOOST_RANGE_COPY_IF
+#include<boost/range/algorithm/copy_if.hpp>
+#else
+#include<algorithm>
+#endif
+namespace srook{
+namespace adaptors{
+inline namespace v1{
+
+template<class Iterator,class Predicate>
+struct copy_if_t{
+	explicit constexpr copy_if_t(Iterator iter,Predicate pred)
+		:iter_(std::move(iter)),pred_(std::move(pred)){}
+	template<class Range>
+	srook::range_iterator<typename std::decay_t<Range>::const_iterator>
+	operator ()(Range&& r)
+	{
+#ifdef POSSIBLE_TO_BOOST_RANGE_COPY_IF
+		boost::copy_if(std::forward<Range>(r),std::move(iter_),std::move(pred_));
+#else
+		std::copy_if(r.cbegin(),r.cend(),std::move(iter_),std::move(pred_));
+#endif
+		return make_range_iterator(r.cbegin(),r.cend());
+	}
+private:
+	Iterator iter_;
+	Predicate pred_;
+};
+template<class Iterator,class Predicate>
+copy_if_t<std::decay_t<Iterator>,std::decay_t<Predicate>> copy_if(Iterator&& iter,Predicate&& pred)
+{
+	return copy_if_t<std::decay_t<Iterator>,std::decay_t<Predicate>>(std::forward<Iterator>(iter),std::forward<Predicate>(pred));
+}
+
+} // inline namespace v1
+} // namespace adaptors
+} // namespace srook
+#ifdef POSSIBLE_TO_BOOST_RANGE_COPY_IF
+#undef POSSIBLE_TO_BOOST_RANGE_COPY_IF
+#endif
+#endif
