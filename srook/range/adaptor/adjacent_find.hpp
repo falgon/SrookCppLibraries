0e81b8dd2628ce1c44a1f9bb2b94f80b5f99b7ad
falgon <falgon53@yahoo.co.jp>
2017-02-14 23:07:27 +0900

    Correspondence for ci

--- srook/range/adaptor/adjacent_find.hpp
+++ srook/range/adaptor/adjacent_find.hpp
@@ -5,12 +5,7 @@
 #include<srook/iterator/range_iterator.hpp>
 #include<srook/config/require.hpp>
 #include<srook/type_traits/is_callable.hpp>
-#if __has_include(<boost/range/algorithm/adjacent_find.hpp>)
-#define POSSIBLE_TO_BOOST_ADJACENT_FIND
-#include<boost/range/algorithm/adjacent_find.hpp>
-#else
 #include<algorithm>
-#endif
 
 namespace srook{
 namespace adaptors{
@@ -25,11 +20,7 @@ struct adjacent_find_t_predicate{
 	template<class Range,REQUIRES(has_iterator_v<std::decay_t<Range>> || is_range_iterator_v<std::decay_t<Range>>)>
 	typename std::decay_t<Range>::const_iterator operator()(Range&& r)
 	{
-#ifdef POSSIBLE_TO_BOOST_ADJACENT_FIND
-		return boost::range::adjacent_find(std::forward<Range>(r),std::move(pred_));
-#else
 		return std::adjacent_find(r.cbegin(),r.cend(),std::move(pred_));
-#endif
 	}
 private:
 	Predicate pred_;
@@ -44,11 +35,7 @@ struct adjacent_find_t{
 	template<class Range>
 	typename std::decay_t<Range>::const_iterator operator()(Range&& r)
 	{
-#ifdef POSSIBLE_TO_BOOST_ADJACENT_FIND
-		return boost::range::adjacent_find(std::forward<Range>(r));
-#else
 		return std::adjacent_find(r.cbegin(),r.cend());
-#endif
 	}
 };
 constexpr adjacent_find_t adjacent_find()
