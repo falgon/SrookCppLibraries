ac0863d27dd56f364de5fb7c738e583df6d08109
falgon <falgon53@yahoo.co.jp>
2017-03-07 10:19:33 +0900

    rename

--- srook/mpl/constant_sequence/equ_sequence.hpp
+++ srook/mpl/constant_sequence/equ_sequence.hpp
@@ -1,5 +1,5 @@
-#ifndef INCLUDED_SROOK_MPL_CONSTANT_SEQUENCE_DOUBLE_SEQUENCE_HPP
-#define INCLUDED_SROOK_MPL_CONSTANT_SEQUENCE_DOUBLE_SEQUENCE_HPP
+#ifndef INCLUDED_SROOK_MPL_CONSTANT_EQU_DOUBLE_SEQUENCE_HPP
+#define INCLUDED_SROOK_MPL_CONSTANT_EQU_DOUBLE_SEQUENCE_HPP
 #include<utility>
 #include<type_traits>
 #include<srook/mpl/constant_sequence/algorithm/concat.hpp>
@@ -11,20 +11,20 @@ namespace constant_sequence{
 inline namespace v1{
 
 template<std::size_t,class>
-struct double_sequence;
+struct make_equ_sequence_impl;
 
 template<std::size_t n,std::size_t... seq>
-struct double_sequence<n,std::index_sequence<seq...>>{
-	using type=concat_t<typename double_sequence<n-1,std::index_sequence<seq...>>::type,make_samevalue_sequence<n,n>>;
+struct make_equ_sequence_impl<n,std::index_sequence<seq...>>{
+	using type=concat_t<typename make_equ_sequence_impl<n-1,std::index_sequence<seq...>>::type,make_samevalue_sequence<n,n>>;
 };
 
 template<std::size_t... seq>
-struct double_sequence<0,std::index_sequence<seq...>>{
+struct make_equ_sequence_impl<0,std::index_sequence<seq...>>{
 	using type=std::index_sequence<0,seq...>;
 };
 
 template<std::size_t n,class Seq=std::index_sequence<>>
-using double_sequence_t=typename double_sequence<n,Seq>::type;
+using make_equ_sequence=typename make_equ_sequence_impl<n,Seq>::type;
 
 } // inline namespace v1
 } // namespace constant_sequence
