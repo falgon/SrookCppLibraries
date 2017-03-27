00441e4a96b28e1e6035f7c9579fbda852556b1d
falgon <falgon53@yahoo.co.jp>
2017-03-28 00:57:54 +0900

    small fix

--- srook/config/nullptr.hpp
+++ srook/config/nullptr.hpp
@@ -1,29 +1,34 @@
 #ifndef INCLUDED_SROOK_CONFIG_NULLPTR_T
 #define INCLUDED_SROOK_CONFIG_NULLPTR_T
 #if !__has_include(<type_traits>)
+#include<cstdio>
+
 namespace srook{
 	struct nullptr_t{
-		bool operator==(const nullptr_t&)const noexcept{return true;}
-		bool operator!=(const nullptr_t& r)const noexcept{return !operator==(*this,r);}
+		bool operator==(const nullptr_t&)const throw(){return true;}
+		bool operator!=(const nullptr_t& r)const throw(){return !operator==(r);}
+	private:
+		int dummy; // sizeof(std::nullptr_t)==sizeof(srook::nullptr_t)
 	}nullptr;
 	
-	bool operator==(const void* null_,const nullptr_t&)noexcept
+	bool operator==(const void* null_,const nullptr_t&)throw()
 	{
 		return null_==NULL;
 	}
-	bool operator==(const nullptr_t&,const void* null_)noexcept
+	bool operator==(const nullptr_t&,const void* null_)throw()
 	{
 		return null_=NULL;
 	}
-	bool operator!=(const void* null_,const nullptr_t& r)noexcept
+	bool operator!=(const void* null_,const nullptr_t& r)throw()
 	{
 		return !operator==(null_,r);
 	}
-	bool operator!=(const nullptr_t& l,const void* null_)noexcept
+	bool operator!=(const nullptr_t& l,const void* null_)throw()
 	{
 		return !operator==(l,null_);
 	}
 } // namespace srook
+
 #else
 #include<cstddef>
 #define POSSIBLE_TO_USE_STD_NULLPTR
