9ffb8c2d9edc62fa07f06b29431a2af16f15eba7
falgon <falgon53@yahoo.co.jp>
2017-04-11 21:29:40 +0900

    add byte

---
+++ srook/config/byte.hpp
@@ -0,0 +1,75 @@
+#ifndef INCLUDED_SROOK_CONFIG_BYTE_HPP
+#define INCLUDED_SROOK_CONFIG_BYTE_HPP
+
+namespace srook{
+
+enum class byte:unsigned char{};
+
+template<class IntegerType>
+constexpr IntegerType to_integer(byte b)noexcept
+{
+	return IntegerType(b);
+}
+
+template<class IntegerType>
+constexpr byte& operator<<=(byte& b,IntegerType shift)noexcept
+{
+	return b=byte(static_cast<unsigned char>(b) << shift);
+}
+
+template <class IntegerType>
+constexpr byte& operator>>=(byte& b,IntegerType shift)noexcept
+{
+	return b=byte(static_cast<unsigned char>(b) >> shift);
+}
+
+template <class IntegerType>
+constexpr byte operator<<(byte b,IntegerType shift)noexcept
+{
+	return byte(static_cast<unsigned char>(b) << shift);
+}
+
+template <class IntegerType>
+constexpr byte operator>>(byte b,IntegerType shift)noexcept
+{
+	return byte(static_cast<unsigned char>(b) >> shift);
+}
+
+constexpr byte& operator|=(byte& l,byte r)noexcept
+{
+	return l = byte(static_cast<unsigned char>(l) | static_cast<unsigned char>(r));
+}
+	
+constexpr byte& operator&=(byte& l,byte r)noexcept
+{
+	return l = byte(static_cast<unsigned char>(l) & static_cast<unsigned char>(r));
+}
+
+constexpr byte& operator^=(byte& l,byte r) noexcept
+{
+	return l = byte(static_cast<unsigned char>(l) ^ static_cast<unsigned char>(r));
+}
+
+constexpr byte operator|(byte l, byte r) noexcept
+{
+	return byte(static_cast<unsigned char>(l) | static_cast<unsigned char>(r));
+}
+
+constexpr byte operator&(byte l, byte r) noexcept
+{
+	return byte(static_cast<unsigned char>(l) & static_cast<unsigned char>(r));
+}
+
+constexpr byte operator^(byte l, byte r) noexcept
+{
+	return byte(static_cast<unsigned char>(l) ^ static_cast<unsigned char>(r));
+}
+
+constexpr byte operator~(byte b) noexcept
+{
+	return byte(~static_cast<unsigned char>(b));
+}
+
+} // namespace srook
+
+#endif
