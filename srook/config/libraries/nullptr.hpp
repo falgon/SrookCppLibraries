3ebc5c7a1f8a0167eb9ae579213e7a68a7e3f540
falgon <falgon53@yahoo.co.jp>
2017-09-28 19:21:08 +0900

    small fix nullptr

--- srook/config/libraries/nullptr.hpp
+++ srook/config/libraries/nullptr.hpp
@@ -2,20 +2,16 @@
 #ifndef INCLUDED_SROOK_CONFIG_LIBRARIES_NULLPTR_HPP
 #define INCLUDED_SROOK_CONFIG_LIBRARIES_NULLPTR_HPP
 
-#include<srook/config/cpp_predefined/feature_testing.hpp>
-#include<srook/config/cpp_predefined/__cplusplus_constant.hpp>
-#include<srook/config/cpp_predefined/macro_names.hpp>
+#include <srook/config/cpp_predefined/__cplusplus_constant.hpp>
+#include <srook/config/cpp_predefined/macro_names.hpp>
 
-#if !SROOK_CPP_LIB_IS_NULL_POINTER
+#if SROOK_CPLUSPLUS11 <= SROOK_CPLUSPLUS
 #	define SROOK_NULLPTR nullptr
 #	define SROOK_NULLPTR_T std::nullptr_t
-#elif SROOK_CPP_LIB_IS_NULL_POINTER && !defined(__GNUC__)
-#	include<srook/config/nullptr.hpp>
+#else
+#	include <srook/config/nullptr.hpp>
 #	define SROOK_NULLPTR srook::nullptr
 #	define SROOK_NULLPTR_T srook::nullptr_t
-#else
-#	define SROOK_NULLPTR nullptr
-#	define SROOK_NULLPTR_T std::nullptr_t
 #endif
 
 #endif
--- srook/config/nullptr.hpp
+++ srook/config/nullptr.hpp
@@ -1,33 +1,20 @@
 // Copyright (C) 2017 roki
 #ifndef INCLUDED_SROOK_CONFIG_NULLPTR_T
 #define INCLUDED_SROOK_CONFIG_NULLPTR_T
-#include<srook/config/cpp_predefined/feature_testing.hpp>
 
 namespace srook {
-struct nullptr_t {
-    bool operator==(const nullptr_t &) const throw() { return true; }
-    bool operator!=(const nullptr_t &r) const throw() { return !operator==(r); }
 
+const class nullptr_t{
+public:
+	template <class T>
+	operator T*() const { return 0; }
+
+	template <class C, class T>
+	operator T C::*() const { return 0; }
 private:
-    int dummy; // sizeof(std::nullptr_t)==sizeof(srook::nullptr_t)
-}nullptr;
+	void operator&() const;
+} nullptr = {};
 
-bool operator==(const void *null_, const nullptr_t &) throw()
-{
-    return null_ == NULL;
-}
-bool operator==(const nullptr_t &, const void *null_) throw()
-{
-    return null_ = NULL;
-}
-bool operator!=(const void *null_, const nullptr_t &r) throw()
-{
-    return !operator==(null_, r);
-}
-bool operator!=(const nullptr_t &l, const void *null_) throw()
-{
-    return !operator==(l, null_);
-}
 } // namespace srook
 
 #endif
