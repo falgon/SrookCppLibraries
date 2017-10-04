1de66ee2c998530b7a5046609673ea8c8d14445a
falgon <falgon53@yahoo.co.jp>
2017-10-04 13:24:31 +0900

    using builtin strlen when set frag

---
+++ srook/string/config/builtin.hpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2017 roki
+#ifndef INCLUDED_SROOK_STRING_CONFIG_BUILTIN_HPP
+#define INCLUDED_SROOK_STRING_CONFIG_BUILTIN_HPP
+
+#include <srook/config/user_config.hpp>
+
+#if !defined(SROOK_CONFIG_DISABLE_BUILTIN_STRING_FUNCTION) && !defined(__clang__)
+#define SROOK_USE_BUILTIN_STRING_FUNCTION 1
+#else
+#define SROOK_USE_BUILTIN_STRING_FUNCTION 0
+#endif
+
+#endif
