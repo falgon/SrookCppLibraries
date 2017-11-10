b881890b3e662a8676e6ce8cf5f6ef1fd2ba7b07
falgon <falgon53@gmail.com>
2017-11-11 00:41:24 +0900

    add some utilities of preprocessor

---
+++ srook/preprocessor/bool.hpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2017 roki
+#ifndef INCLUDED_SROOK_PREPROCESSOR_BOOL_HPP
+#define INCLUDED_SROOK_PREPROCESSOR_BOOL_HPP
+
+#include <srook/preprocessor/not.hpp>
+
+#define SROOK_PP_BOOL(X) SROOK_PP_NOT(SROOK_PP_NOT(X))
+
+#endif
---
+++ srook/preprocessor/not.hpp
@@ -0,0 +1,19 @@
+// Copyright (C) 2017 roki
+#ifndef INCLUDED_SROOK_PREPROCESSOR_NOT_HPP
+#define INCLUDED_SROOK_PREPROCESSOR_NOT_HPP
+
+#include <srook/preprocessor/at.hpp>
+#include <srook/preprocessor/concat.hpp>
+
+#ifdef _MSC_VER
+#	define SROOK_PP_INDIRECT(X) X
+#	define SROOK_PP_IS(...) SROOK_PP_INDIRECT(SROOK_PP_AT_2(__VA_ARGS__, 0))
+#else
+#	define SROOK_PP_IS(...) SROOK_PP_AT_2(__VA_ARGS__, 0)
+#endif
+#define SROOK_PP_PROBE() ~, 1
+
+#define SROOK_PP_NOT(X) SROOK_PP_IS(SROOK_PP_CONCAT(SROOK_PP_NOT_, X))
+#define SROOK_PP_NOT_0 SROOK_PP_PROBE()
+
+#endif
