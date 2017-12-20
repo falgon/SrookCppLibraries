53c51ed3fd18087e308ffaad108386cac22af27f
falgon <falgon53@gmail.com>
2017-12-21 03:30:13 +0900

    add intrusive and remove extra files

---
+++ srook/detail/config_end.hpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2017 roki
+#ifndef INCLUDED_SROOK_INTRUSIVE_DETAIL_CONFIG_END_HPP
+#define INCLUDED_SROOK_INTRUSIVE_DETAIL_CONFIG_END_HPP
+
+#ifdef SROOK_MSVCRT
+#   pragma warning (pop)
+#endif
+
+#endif
