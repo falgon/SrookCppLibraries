2045d8667cad347a5764dacc7eae0b077456c63a
falgon <falgon53@gmail.com>
2018-01-12 01:24:55 +0900

    small fix and renewal

--- srook/algorithm/back.hpp
+++ srook/algorithm/back.hpp
@@ -1,21 +1,70 @@
 // Copyright (C) 2011-2018 Roki. Distributed under the MIT License
-#ifndef INCLUDED_EMPLACE_BACK_HPP
-#define INCLUDED_EMPLACE_BACK_HPP
+#ifndef INCLUDED_ALGORITHM_EMPLACE_BACK_HPP
+#define INCLUDED_ALGORITHM_EMPLACE_BACK_HPP
+
+#ifdef _MSC_VER
+#   if _MSC_VER > 1000
+#       pragma once
+#   endif
+#endif
+
+#include <srook/config.hpp>
+#if SROOK_CPLUSPLUS >= SROOK_CPLUSPLUS11_CONSTANT
+#include <srook/type_traits.hpp>
+
+SROOK_NESTED_NAMESPACE(srook, algorithm) {
+SROOK_INLINE_NAMESPACE(v1)
+
+namespace detail {
+
+template <class T, class... Args>
+struct has_emplace_back {
+private:
+    static SROOK_TRUE_TYPE test(SROOK_DEDUCED_TYPENAME voider<SROOK_DECLTYPE(declval<T>().emplace_back(declval<Args>()...))>::type* = 0);
+    static SROOK_FALSE_TYPE test(...);
+public:
+    static SROOK_CONSTEXPR_OR_CONST bool value = SROOK_DECLTYPE(test(0))::value;
+};
+
+template <class T, class Arg>
+struct has_push_back {
+private:
+    static SROOK_TRUE_TYPE test(SROOK_DEDUCED_TYPENAME add_pointer<SROOK_DECLTYPE(declval<T>().push_back(declval<Arg>()))>::type = 0);
+    static SROOK_FALSE_TYPE test(...);
+public:
+    static SROOK_CONSTEXPR_OR_CONST bool value = SROOK_DECLTYPE(test(0))::value;
+};
+
+} // namespace detail
+
+#if SROOK_CPP_RVALUE_REFERENCES
+#if SROOK_CPP_VARIADIC_TEMPLATES
+template <class Container, class... Args>
+SROOK_FORCE_INLINE 
+SROOK_DEDUCED_TYPENAME enable_if<detail::has_emplace_back<SROOK_DEDUCED_TYPENAME decay<Container>::type, Args&&...>::value>::type
+emplace_back(Container& container, Args&&... args)
+{
+    container.emplace_back(srook::forward<Args>(args)...);
+}
+#endif
+template <class Container, class Add_Element>
+SROOK_FORCE_INLINE 
+SROOK_DEDUCED_TYPENAME enable_if<detail::has_push_back<SROOK_DEDUCED_TYPENAME decay<Container>::type, Add_Element&&>::value>::type
+push_back(Container& container, Add_Element&& args)
+{
+    container.push_back(srook::forward<Add_Element>(args));
+}
+#endif
+SROOK_INLINE_NAMESPACE_END
+
+} SROOK_NESTED_NAMESPACE_END(algorithm, srook)
 
 namespace srook {
-inline namespace v1 {
-template <class Container, class... Args>
-inline void emplace_back(Container &container, Args &&... args)
-{
-    container.emplace_back(args...);
-}
 
-template <class Container, class Add_Element>
-inline void push_back(Container &container, Add_Element &&args)
-{
-    container.push_back(args);
-}
-} // namespace v1
+using srook::algorithm::emplace_back;
+using srook::algorithm::push_back;
+
 } // namespace srook
 
+#endif
 #endif
--- srook/algorithm/emplace_backer.hpp
+++ srook/algorithm/emplace_backer.hpp
@@ -1,23 +1,72 @@
 // Copyright (C) 2011-2018 Roki. Distributed under the MIT License
-#ifndef INCLUDED_EMPLACE_BACKER_HPP
-#define INCLUDED_EMPLACE_BACKER_HPP
-#include <utility>
+#ifndef INCLUDED_SROOK_ALGORITHM_EMPLACE_BACKER_HPP
+#define INCLUDED_SROOK_ALGORITHM_EMPLACE_BACKER_HPP
+
+#ifdef _MSC_VER
+#   if _MSC_VER > 1000
+#       pragma once
+#   endif
+#endif
+
+#include <srook/config.hpp>
+#if SROOK_CPLUSPLUS >= SROOK_CPLUSPLUS11_CONSTANT
+#include <srook/algorithm/back.hpp>
+
+SROOK_NESTED_NAMESPACE(srook, algorithm) {
+SROOK_INLINE_NAMESPACE(v1)
+
+#if SROOK_CPP_FOLD_EXPRESSIONS
+template <class Container, class... Args>
+SROOK_FORCE_INLINE 
+SROOK_DEDUCED_TYPENAME enable_if<detail::has_emplace_back<Container, Args...>::value>::type
+emplace_backer(Container&& v, Args&&... args)
+{
+    (v.emplace_back(srook::forward<Args>(args)), ...);
+}
+
+template <class Container, class... Args>
+SROOK_FORCE_INLINE
+SROOK_DEDUCED_TYPENAME enable_if<detail::has_push_back<Container, Args...>::value>::type
+push_backer(Container&& v, Args&&... args)
+{
+    (v.push_back(srook::forward<Args>(args)), ...);
+}
+#else
+template <class Container>
+SROOK_FORCE_INLINE 
+SROOK_DEDUCED_TYPENAME enable_if<detail::has_emplace_back<Container>::value>::type
+emplace_backer(Container&&) {}
+
+template <class Container, class Arg, class... Args>
+SROOK_DEDUCED_TYPENAME enable_if<detail::has_emplace_back<Container, Arg, Args...>::value>::type
+emplace_backer(Container&& v, Arg&& arg, Args&&... args)
+{
+    v.emplace_back(srook::forward<Arg>(arg));
+    srook::algorithm::emplace_backer(srook::forward<Container>(v), srook::forward<Args>(args)...);
+}
+
+template <class Container>
+SROOK_FORCE_INLINE void push_backer(Container&&) {}
+
+template <class Container, class Arg, class... Args>
+SROOK_DEDUCED_TYPENAME enable_if<detail::has_push_back<Container, Arg, Args...>::value>::type
+push_backer(Container&& v, Arg&& arg, Args&&... args)
+{
+    v.push_back(srook::forward<Arg>(arg));
+    srook::algorithm::emplace_backer(srook::forward<Container>(v), srook::forward<Args>(args)...);
+}
+#endif
+
+SROOK_INLINE_NAMESPACE_END
+} SROOK_NESTED_NAMESPACE_END(algorithm, srook)
 
 namespace srook {
-inline namespace v1 {
 
-template <class Container, class... Args>
-constexpr void emplace_backer(Container &&v, Args &&... args)
-{
-    (v.emplace_back(std::forward<Args>(args)), ...);
-}
-template <class Container, class... Args>
-constexpr void push_backer(Container &&v, Args &&... args)
-{
-    (v.push_back(std::forward<Args>(args)), ...);
-}
+using srook::algorithm::emplace_backer;
+using srook::algorithm::push_backer;
 
-} // namespace v1
 } // namespace srook
+
+#endif
 
 #endif
