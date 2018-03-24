b94685d0a75fca9446cbb0eba89ce445e6cbadea
falgon <falgon53@gmail.com>
2018-03-24 15:38:19 +0900

    fix this: https://travis-ci.org/falgon/SrookCppLibraries/builds/356534025

--- travis/linux/before_install.sh
+++ travis/linux/before_install.sh
@@ -3,6 +3,7 @@
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then 
     sudo apt-get remove -qq -y postgresql-9.1 postgresql-9.2 postgresql-9.3 postgresql-9.4 postgresql-9.6 postgresql-contrib-9.6
     sudo apt-get -qq -y update
+    sudo add-apt-repository -r ppa:webupd8team/java -y
     sudo apt-get -qq upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --allow-unauthenticated
     sudo apt-get -qq dist-upgrade -y; 
 fi
