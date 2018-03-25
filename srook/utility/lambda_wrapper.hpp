82682a1d2692747da03360340135230b981674af
falgon <falgon53@gmail.com>
2018-03-26 03:43:00 +0900

    move lambda_wrapper

--- srook/utility/lambda_wrapper.hpp
+++ srook/utility/lambda_wrapper.hpp
@@ -1,59 +1,21 @@
 // Copyright (C) 2011-2018 Roki. Distributed under the MIT License
 #ifndef INCLUDED_SROOK_UTILITY_LAMBDA_WRAPPER_HPP
 #define INCLUDED_SROOK_UTILITY_LAMBDA_WRAPPER_HPP
-#include <srook/config/cpp_predefined.hpp>
 
-#if SROOK_CPLUSPLUS >= SROOK_CPLUSPLUS11_CONSTANT
-#    include <srook/config/feature/inline_namespace.hpp>
-#    include <srook/config/require.hpp>
-#    include <srook/type_traits/decay.hpp>
-#    include <srook/type_traits/is_callable.hpp>
-#    include <srook/utility/declval.hpp>
-#    include <srook/utility/forward.hpp>
+#include <srook/iterator/range_iterators/detail/lambda_wrapper.hpp>
 
-namespace srook {
-namespace utility {
+SROOK_NESTED_NAMESPACE(srook, utility) {
 SROOK_INLINE_NAMESPACE(v1)
 
-template <class Lambda>
-struct lambda_wrapper {
-    lambda_wrapper(const Lambda& lambda) : lambda_(lambda) {}
-
-    template <class Lambdas, REQUIRES(is_callable<typename decay<Lambdas>::type>::value)>
-    lambda_wrapper& operator=(const Lambdas& lambda)
-    {
-        reset(lambda);
-        return *this;
-    }
-
-    template <class Lambdas, REQUIRES(is_callable<typename decay<Lambdas>::type>::value)>
-    void reset(const Lambdas& lambda)
-    {
-        lambda_.~Lambda();
-        new (&lambda_) typename decay<Lambdas>::type(lambda);
-    }
-
-    template <class... Args>
-    auto operator()(Args&&... value) const
-        -> SROOK_DECLTYPE(Lambda()(declval<Args>()...))
-    {
-        return lambda_(srook::forward<Args>(value)...);
-    }
-
-private:
-    Lambda lambda_;
-};
-
-template <class Lambda>
-lambda_wrapper<typename decay<Lambda>::type> make_lambda_wrapper(Lambda&& lambda)
-{
-    return lambda_wrapper<typename decay<Lambda>::type>(srook::forward<Lambda>(lambda));
-}
+using srook::range::iterator::detail::lambda_wrapper;
 
 SROOK_INLINE_NAMESPACE_END
-} // namespace utility
+} SROOK_NESTED_NAMESPACE_END(utility, srook)
 
-using utility::lambda_wrapper;
+namespace srook {
+
+using srook::utility::lambda_wrapper;
+
 } // namespace srook
-#    endif
+
 #endif
