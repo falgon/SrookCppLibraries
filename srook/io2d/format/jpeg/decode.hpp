98dc6eb48c167f319be72626488cbebfa8940c7f
falgon <falgon53@gmail.com>
2018-04-08 18:53:18 +0900

    move submodule path

--- srook/io2d/format/jpeg/decode.hpp
+++ srook/io2d/format/jpeg/decode.hpp
@@ -2,7 +2,7 @@
 #ifndef INCLUDED_SROOK_IO2D_JPEG_DECODE_HPP
 #define INCLUDED_SROOK_IO2D_JPEG_DECODE_HPP
 
-#include <srook/io2d/jpeg/jpezy/src/decoder/decode_io.hpp>
-#include <srook/io2d/jpeg/jpezy/src/decoder/jpezy_decoder.hpp>
+#include <srook/io2d/format/jpeg/jpezy/src/decoder/decode_io.hpp>
+#include <srook/io2d/format/jpeg/jpezy/src/decoder/jpezy_decoder.hpp>
 
 #endif
