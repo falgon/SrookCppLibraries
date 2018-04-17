455632ae2dc7ffaf1c0098d70cc63f5e72539923
falgon <falgon53@gmail.com>
2018-04-18 00:50:01 +0900

    add difference types sorts

---
+++ srook/tmpl/vt/sort/isort.hpp
@@ -0,0 +1,62 @@
+// Copyright (C) 2011-2018 Roki. Distributed under the MIT License
+#ifndef INCLUDED_SROOK_TMPL_VT_ISORT_HPP
+#define INCLUDED_SROOK_TMPL_VT_ISORT_HPP
+
+#ifdef _MSC_VER
+#   if _MSC_VER > 1000
+#       pragma once
+#   endif
+#endif
+
+#include <srook/tmpl/vt/detail/config.hpp>
+#include <srook/tmpl/vt/compare.hpp>
+#include <srook/tmpl/vt/concat.hpp>
+#include <srook/type_traits/type_constant.hpp>
+#include <srook/type_traits/conditional.hpp>
+
+SROOK_NESTED_NAMESPACE(srook, tmpl, vt) {
+SROOK_INLINE_NAMESPACE(v1)
+
+template <class, template <class, class> class = srook::tmpl::vt::lt>
+struct isort;
+
+namespace detail {
+
+template <template <class, class> class, class, class>
+struct insert;
+
+template <template <class, class> class Op, class X>
+struct insert<Op, X, packer<>> : type_constant<packer<X>> {};
+
+template <template <class, class> class Op, class X, class Y, class... Ys>
+struct insert<Op, X, packer<Y, Ys...>> 
+    : conditional<
+        Op<X, Y>::type::value, 
+        packer<X, Y, Ys...>,
+        SROOK_DEDUCED_TYPENAME concat<Y, SROOK_DEDUCED_TYPENAME insert<Op, X, packer<Ys...>>::type>::type
+    > {};
+
+template <template <class, class> class, class>
+struct insert_sort;
+
+template <template <class, class> class Op>
+struct insert_sort<Op, packer<>> : type_constant<packer<>> {};
+
+template <template <class, class> class Op, class X, class... Xs>
+struct insert_sort<Op, packer<X, Xs...>> 
+    : insert<Op, X, SROOK_DEDUCED_TYPENAME insert_sort<Op, packer<Xs...>>::type> {};
+
+} // namespace detail
+
+template <class... Xs, template <class, class> class Compare>
+struct isort<packer<Xs...>, Compare> 
+    : detail::insert_sort<Compare, packer<Xs...>> {};
+
+#if SROOK_CPP_ALIAS_TEMPLATES
+template <class Xs, template <class, class> class Compare = srook::tmpl::vt::lt>
+using isort_t = SROOK_DEDUCED_TYPENAME isort<Xs, Compare>::type;
+#endif
+
+SROOK_INLINE_NAMESPACE_END
+} SROOK_NESTED_NAMESPACE_END(vt, tmpl, srook)
+#endif
---
+++ srook/tmpl/vt/sort/msort.hpp
@@ -0,0 +1,77 @@
+// Copyright (C) 2011-2018 Roki. Distributed under the MIT License
+#ifndef INCLUDED_SROOK_TMPL_VT_MSORT_HPP
+#define INCLUDED_SROOK_TMPL_VT_MSORT_HPP
+
+#ifdef _MSC_VER
+#   if _MSC_VER > 1000
+#       pragma once
+#   endif
+#endif
+
+#include <srook/tmpl/vt/detail/config.hpp>
+#include <srook/tmpl/vt/compare.hpp>
+#include <srook/tmpl/vt/concat.hpp>
+#include <srook/tmpl/vt/take.hpp>
+#include <srook/tmpl/vt/drop.hpp>
+#include <srook/type_traits/type_constant.hpp>
+#include <srook/type_traits/conditional.hpp>
+
+SROOK_NESTED_NAMESPACE(srook, tmpl, vt) {
+SROOK_INLINE_NAMESPACE(v1)
+
+template <class, template <class, class> class = srook::tmpl::vt::lt>
+struct msort;
+
+namespace detail {
+
+template <class, class, template <class, class> class>
+struct merge;
+
+template <template <class, class> class Compare>
+struct merge<packer<>, packer<>, Compare> : type_constant<packer<>> {};
+
+template <class X, class... Xs, template <class, class> class Compare>
+struct merge<packer<X, Xs...>, packer<>, Compare> : type_constant<packer<X, Xs...>> {};
+
+template <class Y, class... Ys, template <class, class> class Compare>
+struct merge<packer<>, packer<Y, Ys...>, Compare> : type_constant<packer<Y, Ys...>> {};
+
+template <class X, class... Xs, class Y, class... Ys, template <class, class> class Compare>
+struct merge<packer<X, Xs...>, packer<Y, Ys...>, Compare> 
+    : conditional<
+        Compare<X, Y>::type::value,
+        SROOK_DEDUCED_TYPENAME concat<X, SROOK_DEDUCED_TYPENAME merge<packer<Xs...>, packer<Y, Ys...>, Compare>::type>::type,
+        SROOK_DEDUCED_TYPENAME concat<Y, SROOK_DEDUCED_TYPENAME merge<packer<X, Xs...>, packer<Ys...>, Compare>::type>::type
+    > {};
+
+template <class, template <class, class> class>
+struct merge_sort;
+
+template <template <class, class> class Compare>
+struct merge_sort<packer<>, Compare> : type_constant<packer<>> {};
+
+template <class X, template <class, class> class Compare>
+struct merge_sort<packer<X>, Compare> : type_constant<packer<X>> {};
+
+template <class... Xs, template <class, class> class Compare>
+struct merge_sort<packer<Xs...>, Compare> 
+    : merge<
+        SROOK_DEDUCED_TYPENAME merge_sort<SROOK_DEDUCED_TYPENAME take<sizeof...(Xs) / 2, Xs...>::type, Compare>::type,
+        SROOK_DEDUCED_TYPENAME merge_sort<SROOK_DEDUCED_TYPENAME drop<sizeof...(Xs) / 2, Xs...>::type, Compare>::type,
+        Compare
+    > {};
+
+} // namespace detail
+
+template <class... Xs, template <class, class> class Compare>
+struct msort<packer<Xs...>, Compare> 
+    : detail::merge_sort<packer<Xs...>, Compare> {};
+
+#if SROOK_CPP_ALIAS_TEMPLATES
+template <class Xs, template <class, class> class Compare = srook::tmpl::vt::lt>
+using msort_t = SROOK_DEDUCED_TYPENAME msort<Xs, Compare>::type;
+#endif
+
+SROOK_INLINE_NAMESPACE_END
+} SROOK_NESTED_NAMESPACE_END(vt, tmpl, srook)
+#endif
