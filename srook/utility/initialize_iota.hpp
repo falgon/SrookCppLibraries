90dbe9570e5798c664caa626af740aa58d736636
falgon <falgon53@gmail.com>
2018-05-15 22:46:09 +0900

    add include guard

--- srook/utility/initialize_iota.hpp
+++ srook/utility/initialize_iota.hpp
@@ -1,8 +1,10 @@
 // Copyright (C) 2011-2018 Roki. Distributed under the MIT License
 #ifndef INCLUDED_INIT_IOTA
 #define INCLUDED_INIT_IOTA
-#include<boost/preprocessor/repetition/repeat_from_to.hpp>
-#define INIT_IOTA_T(z,x,text) x,
-#define INIT_IOTA(x,y) BOOST_PP_REPEAT_FROM_TO(x,y,INIT_IOTA_T,_)
-#define IIOTA INIT_IOTA
+#include <srook/config/cpp_predefined.hpp>
+#if SROOK_HAS_INCLUDE(<boost/preprocessor/repetition/repeat_from_to.hpp>)
+#   define INIT_IOTA_T(z,x,text) x,
+#   define INIT_IOTA(x,y) BOOST_PP_REPEAT_FROM_TO(x,y,INIT_IOTA_T,_)
+#   define IIOTA INIT_IOTA
+#endif
 #endif
